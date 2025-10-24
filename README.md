2494362d2dd900fccd328404a0058a02bc8b9ef3
cpprian <cpprian456@gmail.com>
2025-10-24 23:25:36 +0200

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# E-commerce FastAPI Backend
+
+This repository contains a backend for an E-commerce application built using FastAPI. It utilizes MongoDB as the main database and Redis for caching to enhance performance.
+
---
+++ app/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGODB_URL: str
+    MONGO_DB_NAME: str
+
+    REDIS_HOST: str = "localhost"
+    REDIST_PORT: int = 6379
+    REDIS_DB: int = 0
+    REDIS_CACHE_EXPIRE: int = 300
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    DEBUG: bool = False
+    APP_NAME: str = "ecommerce-fastapi"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()
